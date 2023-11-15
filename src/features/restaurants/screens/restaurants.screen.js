29d08e043696013d0dc0e40fd5100bc28dc6138b
davis122991 <110353699+davis122991@users.noreply.github.com>
2023-11-15 22:05:53 +0200

    created theme provider

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,23 +1,21 @@
 import styled from 'styled-components';
 import { View, SafeAreaView, StatusBar } from 'react-native';
-import { spacing } from '../../../utils/sizes';
 
 import { Searchbar } from 'react-native-paper';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
 `;
 
 const SearchContainer = styled(View)`
-  padding: ${spacing.md}px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const ReastaurantListContainer = styled(View)`
   flex: 1;
-  padding: ${spacing.md}px;
-  background-color: blue;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => {
