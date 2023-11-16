5905fd12dcfdec6a2d836315405463c58074d35e
davis122991 <110353699+davis122991@users.noreply.github.com>
2023-11-16 19:01:37 +0200

    added google fonts

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+
 import { theme } from './src/infrastructure/theme';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 
 export default function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) null;
+
   return (
     <>
       <ThemeProvider theme={theme}>
