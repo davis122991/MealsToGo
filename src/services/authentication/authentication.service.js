a6cf76622623a4434f94b4bd54291ffc7781c88d
davis122991 <110353699+davis122991@users.noreply.github.com>
2024-01-01 21:18:30 +0200

    Update authentication.service.js

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -3,12 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDj31CwDEgqT9FjQcyyoBHdSy_YiEaOQOg',
-  authDomain: 'mealstogo-bcbf4.firebaseapp.com',
-  projectId: 'mealstogo-bcbf4',
-  storageBucket: 'mealstogo-bcbf4.appspot.com',
-  messagingSenderId: '537977524603',
-  appId: '1:537977524603:web:c2bc89fd617a616a24edde',
+  
 };
 
 const app = initializeApp(firebaseConfig);
